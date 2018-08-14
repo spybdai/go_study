cf021deccd88b7374b735eeb23e4eb880fa4d413
spybdai <spybdai@gmail.com>
2018-08-15 00:11:36 +0800

    add customerized package usage example

---
+++ basic/cust_package/package_example_1.go
@@ -0,0 +1,8 @@
+
+package cust_package
+
+import "fmt"
+
+func Test(){
+	fmt.Printf("test\n")
+}
---
+++ basic/use_cust_package/use_cust_package.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spybdai/go_study/basic/cust_package"
+)
+
+func main(){
+	cust_package.Test()	
+	fmt.Printf("I am also testing\n")
+}
